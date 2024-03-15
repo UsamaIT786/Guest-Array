24c7a75babb774c460e58d6c2a3bc548421cf5a8
Usama Muzammil <160235187+UsamaIT786@users.noreply.github.com>
2024-03-15 18:18:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//Assignment 14
+//Task 14
+var guestarr = ["Usama", "Muzammil", "Bilal", "Arsalan"];
+guestarr.map(function (items) { return console.log("Dear ".concat(items, " you are invited to the dinner?")); });
---
+++ main.ts
@@ -0,0 +1,5 @@
+//Assignment 14
+//Task 14
+
+let guestarr : string[] = ["Usama","Muzammil","Bilal","Arsalan"];
+guestarr.map((items) => console.log(`Dear ${items} you are invited to the dinner?`));
